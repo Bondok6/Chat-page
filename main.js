a86c4b21ad01832dfeef504f987eed352df459a5
Bondok6 <kirolousbondok6@gmail.com>
2021-05-16 00:22:43 +0200

    first

---
+++ main.js
@@ -0,0 +1,36 @@
+const slider = document.querySelector('.slider');
+const sliderImages = document.querySelectorAll('.slider img');
+
+//Buttons
+const prevBtn = document.querySelector('.carousel-control-prev');
+const nextBtn = document.querySelector('.carousel-control-next');
+
+let counter = 1;
+//const size = sliderImages[0].clientWidth;
+const size = 200;
+
+slider.style.transform = 'translateX(' + -size * counter + 'px)';
+
+nextBtn.addEventListener('click', () => {
+	if (counter == sliderImages.length - 5) return;
+	slider.style.transition = 'transform 0.4s ease-in-out';
+	counter++;
+	slider.style.transform = 'translateX(' + -size * counter + 'px)';
+});
+
+prevBtn.addEventListener('click', () => {
+	if (counter <= 0) return;
+	slider.style.transition = 'transform 0.4s ease-in-out';
+	counter--;
+	slider.style.transform = 'translateX(' + -size * counter + 'px)';
+});
+
+//Replay Button
+btnReplay = document.querySelectorAll('.btn-reply');
+inputReplay = document.querySelectorAll('.replay');
+
+for (i = 0; i < btnReplay.length; i++) {
+	btnReplay[i].addEventListener('click', () => {
+		inputReplay[0].style.display = 'block';
+	});
+}
